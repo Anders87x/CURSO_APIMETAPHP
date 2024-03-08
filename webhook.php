864067cd04becb5712f4b75e7270fdefe637b2a1
AnderCode <davisanderson87@gmail.com>
2024-03-08 17:46:43 -0500

    Subiendo Github 4

--- webhook.php
+++ webhook.php
@@ -36,9 +36,11 @@
             fwrite($archivo,$texto);
             fclose($archivo);
 
-            $res ->status(200)->send("EVENT_RECEIVED");
+            /* $res ->status(200)->send("EVENT_RECEIVED"); */
+            $res->status(200)->send(json_encode(['message' => 'EVENT_RECEIVED']));
         }catch(Exception $e){
-            $res ->status(200)->send("EVENT_RECEIVED");
+            $res->status(200)->send(json_encode(['message' => 'EVENT_RECEIVED']));
+            /* $res ->status(200)->send("EVENT_RECEIVED"); */
         }
     }
 
